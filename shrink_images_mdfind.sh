e8f1103319d0804fb8b87d2446514f8788ad5e2e
Jim Covert <loudgazelle@users.noreply.github.com>
2015-10-20 11:31:46 -0700

    Create shrink_images_mdfind.sh

---
+++ shrink_images_mdfind.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# limit our search the current directory and all subdirectories of the current directory
+DIR=$(pwd)
+
+# the maximum length or width (whichever is greater) in pixels.
+# also limits our search to images larger than this
+MAX_SIZE=2590
+
+# tell xargs to run this many sips processes in parallel
+CORES=4
+
+mdfind -0 -onlyin $DIR "kMDItemPixelHeight > $MAX_SIZE || kMDItemPixelWidth > $MAX_SIZE"| xargs -0 -P $CORES $
+
+
